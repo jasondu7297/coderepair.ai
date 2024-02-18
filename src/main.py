867ca4d960d90ed5be3b6b9929f5f4768e2f4f26
Jason Du <jason.moshi.du@gmail.com>
2024-02-18 02:39:21 -0800

    Add python argparsing

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
-import subprocess
 import sys
 from argparse import ArgumentParser, Namespace
 from jobs import Job
 
-def main():
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <executable_path> <compile_cmd>")
-        sys.exit(1)
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
 
+    parser.add_argument(
+        "-e",
+        "--executable",
+        help="Path to the executable to run your test suite",
+    )
+    parser.add_argument(
+        "-c",
+        "--compile-cmd",
+        help="Command to recompile your executable as inputed with -e/--executable flag",
+    )
+
+    args = parser.parse_args()
+
+    if len(vars(args)) < 2:
+        parser.error("both the arguments -e/--executable and -c/--compile-cmd are required")
+
+    return args
+
+def main():
+    args = parse_args()
     job = Job(
-        exec_path=sys.argv[1],
-        compile_cmd=sys.argv[2])
+        exec_path=args.executable,
+        compile_cmd=args.compile_cmd)
     job.execute()
 
 if __name__ == "__main__":
